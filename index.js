eac18ce2355b0b6440fce5ad4d7c5640f276cab4
matthd <matthias.dieudonne@gmail.com>
2016-10-25 15:24:21 +0200

    Start creating npm depot

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-/**
- * Created by dieudonn on 24/10/2016.
- */
+const FromXml = require('./lib/FromXml');
 
-const fromXml = require('./lib/FromXml');
+let xml = new FromXml().generate();
 
-let xml = new fromXml().loadFile('/Users/dieudonn/Documents/INIST/xpath-generator/sage-file-test.xm');
-
-xml.then(function (file) {
-  console.log(file)
-});
