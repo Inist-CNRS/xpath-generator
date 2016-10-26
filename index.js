c6d74438606f3c2f3ffb5965665482aec7f914c1
matthd <matthias.dieudonne@gmail.com>
2016-10-26 11:30:22 +0200

    can now count on folder & return promise

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-/*const FromXml = require('./lib/FromXml');
+const FromXml = require('./lib/FromXml');
 let xml = new FromXml().generate().then(results=>{
   for (var path in results){
     console.log(`${path} ${results[path]}`)
   }
-});*/
+});
 
-const FromFolder = require('./lib/FromFolder');
+/*const FromFolder = require('./lib/FromFolder');
 let xml = new FromFolder().generateAll().then((result)=>{
   console.log(result);
-});
+});*/
 
