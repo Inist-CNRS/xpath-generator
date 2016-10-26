1c36a44ce5a07d724d04a05351accf9a0c262a4d
matthd <matthias.dieudonne@gmail.com>
2016-10-26 14:22:43 +0200

    Start cli

---
+++ app-xpaths-old.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const expat = require('node-expat');
+const _ = require('lodash');
+const parser = new expat.Parser('UTF-8');
+const fs = require('fs');
+
+const data = fs.readFileSync('test-big.xml', 'utf8');
+
+const arrData = [];
+const mapResults = [];
+
+const  xpaths = fs.createWriteStream('xpaths.txt');
+
+console.log(parser)
+
+parser.on('startElement', function (name, attrs) {
+  const attributes = [];
+  let attributeXpath = '';
+  const keys = Object.keys(attrs);
+  if (keys.length > 0) {
+    for (let i = 0; i < keys.length; i++) {
+      attributes.push('@' + keys[i] + "='" + attrs[keys[i]] + "'")
+    }
+    attributeXpath = '[' + attributes.join(' and ') + ']';
+  }
+
+  arrData.push(name);
+  const xpath = arrData.join('/') + attributeXpath;
+  //console.log(xpath);
+  xpaths.write(xpath);
+
+  const getResult = _.find(mapResults, {'xpath': xpath});
+  const haveResult = _.isObject(getResult);
+  if (haveResult) {
+    getResult.count++
+  } else {
+    mapResults.push({
+      xpath: xpath,
+      count: 1
+    });
+  }
+
+  // console.log(mapResults);
+});
+
+parser.on('endElement', function () {
+  arrData.pop();
+});
+
+parser.on('error', function (error) {
+  console.error(error)
+});
+
+parser.write(data);
+// parser.write('<html><head><title>Hello World</title></head><body><p>Foobar</p></body></html>')
--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-/*const FromXml = require('./lib/FromXml');
-let xml = new FromXml().generate().then(results=>{
-  for (var path in results){
-    console.log(`${path} ${results[path].count}`)
-  }
-});*/
+const FromXml = require('./lib/FromXml'),
+      FromFolder = require('./lib/FromFolder');
 
-const FromFolder = require('./lib/FromFolder');
-let xml = new FromFolder().generateAll().then((results)=>{
-  for (var path in results) {
-    console.log(`${'│  '.repeat(results[path].level)}├── ${path} ${results[path].count}`);
-    //console.log(`${path} ${results[path].count} ${results[path].level}` )
-  }
-});
-
+exports.FromXml = FromXml;
+exports.FromFolder = FromFolder;
