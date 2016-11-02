3dddf5e40474d8d5310b99340458a8252b277555
matthd <matthias.dieudonne@gmail.com>
2016-11-02 11:39:07 +0100

    Add many test

--- test/test-xml.js
+++ test/test-xml.js
@@ -2,6 +2,7 @@
 
 const chai = require('chai'),
       expect = chai.expect,
+      fs = require('fs'),
       FromXml = require('../lib/FromXml'),
       FromFolder = require('../lib/FromFolder');
 
@@ -19,37 +20,62 @@ describe('Default XML Class instance params', () => {
     expect(xml._cheerio_opts).to.have.property('xmlMode', true);
 
   });
+})
+describe('Unit error tests on Methods', () => {
   it('Should throw an Error if path not send',(done)=>{
     expect(xml.loadFile).to.throw(Error);
     done();
   })
-  // it('Should use params & return obj', ()=> {
-  //   return xml.loadFile().then(file=>{
-  //     expect(file).to.be.a('string');
-  //     return xml.loadData(file);
-  //   })
-  //   .then(data =>{
-  //     expect(data).to.be.a('function');
-  //     return xml.parse(data,data.root().children());
-  //   })
-  //   .then(()=>{
-  //     expect(xml._count).to.be.a('object');
+  it('Should return an Error if path is not a file',()=>{
+    return xml.loadFile('test/test.xml').catch(err=>{
+      expect(err).to.not.be.empty;
+    })
+  })
+  it('Should throw an Error if data not send',(done)=>{
+    expect(xml.loadData).to.throw(Error);
+    done();
+  })
+  it('Should throw an Error if parse is call without args',(done)=>{
+    expect(xml.parse).to.throw(Error);
+    done();
+  })
+})
+describe('Unit valid test on Methods', () => {
+  var xmlFile = fs.readFileSync('samples/test.xml','utf-8');
+  it('Should return a valid cheerio $', () => {
+    let $ = xml.loadData(xmlFile)
+    expect($()).to.be.a('object');
+    expect($.root().children()).to.be.a('object')
+    expect($.root().children()).to.have.length.above(0)
+  })
+})
+describe('Test on an simple xml', () => {
+  it('Should use params & return obj', ()=> {
+    return xml.loadFile('samples/test.xml').then(file=>{
+      expect(file).to.be.a('string');
+      return xml.loadData(file);
+    })
+    .then(data =>{
+      expect(data).to.be.a('function');
+      return xml.parse(data,data.root().children());
+    })
+    .then(()=>{
+      expect(xml._count).to.be.a('object');
 
-  //     //Usefull to compare if generate() return the same as above
-  //     firstKeyToCheck  = Object.keys(xml._count)[0];
-  //     firstValueToCheck = xml._count[Object.keys(xml._count)[0]];
+      //Usefull to compare if generate() return the same as above
+      firstKeyToCheck  = Object.keys(xml._count)[0];
+      firstValueToCheck = xml._count[Object.keys(xml._count)[0]];
 
-  //     //Reset xmlcount for next test
-  //     xml._count= {};
-  //   });
-  // });
-  // it('Should Generate an obj from xml file from generate method', ()=>{
-  //   return xml.generate('./samples/test.xml').then(result=> {
-  //     let firstKeyToCompare  = Object.keys(result)[0],
-  //         firstValueToCompare = result[Object.keys(result)[0]];
-
-  //     expect(firstKeyToCompare).to.equal(firstKeyToCheck);
-  //     expect(firstValueToCompare).to.deep.equal(firstValueToCheck);
-  //   });
-  // })
-});
+      //Reset xmlcount for next test
+      xml._count= {};
+    });
+  });
+  it('Should Generate an obj from xml file from generate method', ()=>{
+    return xml.generate('./samples/test.xml',true).then(result=> {
+      let firstKeyToCompare  = Object.keys(result)[0],
+          firstValueToCompare = result[Object.keys(result)[0]];
+      expect(firstKeyToCompare).to.equal(firstKeyToCheck);
+      expect(firstValueToCompare).to.deep.equal(firstValueToCheck);
+    });
+  })
+})
