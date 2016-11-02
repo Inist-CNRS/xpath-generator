fb64b41f8e0e8d9fbc96615d9165b2ffc273eb16
matthd <matthias.dieudonne@gmail.com>
2016-11-02 10:51:10 +0100

    Throw error if no path sent

--- test/test-xml.js
+++ test/test-xml.js
@@ -19,33 +19,37 @@ describe('Default XML Class instance params', () => {
     expect(xml._cheerio_opts).to.have.property('xmlMode', true);
 
   });
-  it('Should use default es6 params when no args & return obj', ()=> {
-    return xml.loadFile().then(file=>{
-      expect(file).to.be.a('string');
-      return xml.loadData(file);
-    })
-    .then(data =>{
-      expect(data).to.be.a('function');
-      return xml.parse(data,data.root().children());
-    })
-    .then(()=>{
-      expect(xml._count).to.be.a('object');
-
-      //Usefull to compare if generate() return the same as above
-      firstKeyToCheck  = Object.keys(xml._count)[0];
-      firstValueToCheck = xml._count[Object.keys(xml._count)[0]];
-
-      //Reset xmlcount for next test
-      xml._count= {};
-    });
-  });
-  it('Should generate an obj from xml file', ()=>{
-    return xml.generate('./samples/test.xml').then(result=> {
-      let firstKeyToCompare  = Object.keys(result)[0],
-          firstValueToCompare = result[Object.keys(result)[0]];
-
-      expect(firstKeyToCompare).to.equal(firstKeyToCheck);
-      expect(firstValueToCompare).to.deep.equal(firstValueToCheck);
-    });
+  it('Should throw an Error if path not send',(done)=>{
+    expect(xml.loadFile).to.throw(Error);
+    done();
   })
+  // it('Should use params & return obj', ()=> {
+  //   return xml.loadFile().then(file=>{
+  //     expect(file).to.be.a('string');
+  //     return xml.loadData(file);
+  //   })
+  //   .then(data =>{
+  //     expect(data).to.be.a('function');
+  //     return xml.parse(data,data.root().children());
+  //   })
+  //   .then(()=>{
+  //     expect(xml._count).to.be.a('object');
+
+  //     //Usefull to compare if generate() return the same as above
+  //     firstKeyToCheck  = Object.keys(xml._count)[0];
+  //     firstValueToCheck = xml._count[Object.keys(xml._count)[0]];
+
+  //     //Reset xmlcount for next test
+  //     xml._count= {};
+  //   });
+  // });
+  // it('Should Generate an obj from xml file from generate method', ()=>{
+  //   return xml.generate('./samples/test.xml').then(result=> {
+  //     let firstKeyToCompare  = Object.keys(result)[0],
+  //         firstValueToCompare = result[Object.keys(result)[0]];
+
+  //     expect(firstKeyToCompare).to.equal(firstKeyToCheck);
+  //     expect(firstValueToCompare).to.deep.equal(firstValueToCheck);
+  //   });
+  // })
 });
